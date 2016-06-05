a8b4658e28149a58ad50e2cda4d121f5bf41874b
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-06-05 18:43:46 +0800

    Hi Kotlin

---
+++ learn-kotlin/customer.kt
@@ -0,0 +1,7 @@
+data class Customer(val name: String, val email: String)
+
+fun main(args: Array<String>) {
+    val customer = Customer("Wayan Jimmy", "jimmyeatcrab@gmail.com")
+    println(customer)
+    println("Hello, World!")
+}
