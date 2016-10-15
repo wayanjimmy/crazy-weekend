e315838a8c0ac7ecd101e153e7cbdb6681f5df21
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-10-15 15:49:15 +0800

    Learn redux

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        I'm the Photo Grid
+      </div>
+    );
+  }
+});
+
+export default PhotoGrid;
---
+++ learn-redux/client/reduxstagram.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+import css from './styles/style.styl';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+);
+
+render(router, document.getElementById('root'));
