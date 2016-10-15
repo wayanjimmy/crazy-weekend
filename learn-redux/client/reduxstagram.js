8d5c6930f9b2190c707541ab9085e08ca44f9086
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-10-15 19:38:15 +0800

    Load smart component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import Main from './components/Main';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 import { Router, Route, IndexRoute } from 'react-router';
@@ -12,7 +11,7 @@ import css from './styles/style.styl';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
       </Route>
