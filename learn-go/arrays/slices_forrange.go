5a1d09860548c7220ca0ab973194db77ed341f1e
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-10-30 17:49:14 +0800

    Learn webpack (again)

---
+++ learn-go/arrays/slices_forrange.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice1 := make([]int, 4)
+}
---
+++ learn-webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+}
