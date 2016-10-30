ed6df7fbc3745112a9839fe8324f7e5cd6b38b1b
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-10-31 07:30:37 +0800

    HMR

---
+++ learn-webpack/libs/parts.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      stats: 'errors-only',
+      host: options.host,
+      port: options.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({
+        multistep: true
+      })
+    ]
+  };
+}
--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const parts = require('./libs/parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
     app: PATHS.app
   },
@@ -19,4 +22,22 @@ module.exports = {
       title: 'Webpack demo'
     })
   ]
+};
+
+var config;
+
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(
+      common,
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
+
+module.exports = validate(config);
