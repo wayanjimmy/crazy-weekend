3946767e461ccfd0f838899b09842b7acfce6b28
Wayan Jimmy <jimmyeatcrab@gmail.com>
2016-11-07 14:54:46 +0800

    Handler

---
+++ learn-go/go_in_practice/multiple_handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/goodbye/", goodbye)
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	name := query.Get("name")
+
+	if name == "" {
+		name = "Wayan Jimmy"
+	}
+	fmt.Fprintf(res, "Hello, my name is %s", name)
+}
+
+func goodbye(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+	name := parts[2]
+	if name == "" {
+		name = "Wayan Jimmy"
+	}
+
+	fmt.Fprintf(res, "Goodbye %s", name)
+}
+
+func homePage(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+	fmt.Fprintf(res, "The homepage.")
+}
