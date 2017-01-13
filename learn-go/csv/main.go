2595c2cc3ddfa13c41922bb3ae3a335e469ff703
Wayan Jimmy <jimmyeatcrab@gmail.com>
2017-01-13 07:33:11 +0700

    Read csv

--- learn-go/csv/main.go
+++ learn-go/csv/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -36,4 +37,27 @@ func main() {
 		}
 	}
 	writer.Flush()
+
+	file, err := os.Open("posts.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+	record, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var posts []Post
+	for _, item := range record {
+		id, _ := strconv.ParseInt(item[0], 0, 0)
+		post := Post{Id: int(id), Content: item[1], Author: item[2]}
+		posts = append(posts, post)
+	}
+	fmt.Println(posts[0].Id)
+	fmt.Println(posts[0].Content)
+	fmt.Println(posts[0].Author)
 }
