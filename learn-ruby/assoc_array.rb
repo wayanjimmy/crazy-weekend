1c17926eded36d8fbc0ebed5b05a392feb61fe96
Wayan Jimmy <jimmyeatcrab@gmail.com>
2017-02-05 19:14:18 +0700

    Assoc array

---
+++ learn-ruby/assoc_array.rb
@@ -0,0 +1,21 @@
+inst_section = {
+  :cello => 'string',
+  :clarinet => 'woodwind',
+  :drum => 'percussion',
+  :oboe => 'woodwind',
+  :trumpet => 'brass',
+  :violin => 'string'
+}
+
+puts inst_section[:clarinet]
+
+another_section = {
+  cello: 'string',
+  clarinet: 'woodwind',
+  drum: 'percussion',
+  obboe: 'woodwind',
+  trumpet: 'brass',
+  violin: 'string'
+}
+
+puts another_section[:violin]
