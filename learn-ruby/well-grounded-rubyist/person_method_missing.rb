7a5a58a1a0b3bdacd164048378c18c8ae6dc3520
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-01-15 15:55:18 +0800

    method missing

---
+++ learn-ruby/well-grounded-rubyist/person_method_missing.rb
@@ -0,0 +1,34 @@
+class Person
+  PEOPLE = []
+  attr_reader :name, :hobbies, :friends
+  def initialize(name)
+    @name = name
+    @hobbies = []
+    @friends = []
+    PEOPLE << self
+  end
+
+  def has_hobby(hobby)
+    @hobbies << hobby
+  end
+
+  def has_friend(friend)
+    @friends << friend
+  end
+
+  def self.method_missing(m, *args)
+    method = m.to_s
+    if method.start_with?("all_with_")
+      attr = method[9..-1]
+      if self.public_method_defined?(attr)
+        PEOPLE.find_all do |person|
+          person.send(attr).include?(args[0])
+        end
+      else
+        raise ArgumentError, "Can't find #{attr}"
+      end
+    end
+  end
+end
+
+
