c22986a673060c03c0067b4a703cca0d04a3f015
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-03-23 17:17:44 +0700

    parser

--- tinicompiler/src/tini-compiler.js
+++ tinicompiler/src/tini-compiler.js
@@ -88,6 +88,70 @@ function tokenizer(input) {
   return tokens
 }
 
+function parser(tokens) {
+  let current = 0
+
+  function walk() {
+    let token = tokens[current]
+
+    if (token.type === 'number') {
+      current++
+
+      return {
+        type: 'NumberLiteral',
+        value: token.value
+      }
+    }
+
+    if (token.type === 'string') {
+      current++
+
+      return {
+        type: 'StringLiteral',
+        value: token.value
+      }
+    }
+
+    if (token.type === 'paren' && token.value === '(') {
+      token = tokens[++current]
+
+      let node = {
+        type: 'CallExpression',
+        name: token.value,
+        params: []
+      }
+
+      token = tokens[++current]
+
+      while (
+        token.type !== 'paren' ||
+        (token.type === 'paren' && token.value !== ')')
+      ) {
+        node.params.push(walk())
+        token = tokens[current]
+      }
+
+      current++
+
+      return node
+    }
+
+    throw new TypeError(token.type)
+  }
+
+  let ast = {
+    type: 'Program',
+    body: []
+  }
+
+  while (current < tokens.length) {
+    ast.body.push(walk())
+  }
+
+  return ast
+}
+
 module.exports = {
-  tokenizer
+  tokenizer,
+  parser,
 }
--- tinicompiler/src/tini-compiler.test.js
+++ tinicompiler/src/tini-compiler.test.js
@@ -1,4 +1,4 @@
-const { tokenizer } = require('./tini-compiler')
+const { tokenizer, parser } = require('./tini-compiler')
 
 const input = '(add 2 (subtract 4 2))'
 
@@ -14,6 +14,40 @@ const tokens = [
   { type: 'paren', value: ')' }
 ]
 
+const ast = {
+  type: 'Program',
+  body: [
+    {
+      type: 'CallExpression',
+      name: 'add',
+      params: [
+        {
+          type: 'NumberLiteral',
+          value: '2'
+        },
+        {
+          type: 'CallExpression',
+          name: 'subtract',
+          params: [
+            {
+              type: 'NumberLiteral',
+              value: '4'
+            },
+            {
+              type: 'NumberLiteral',
+              value: '2'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
 it('tokenizer', () => {
   expect(tokenizer(input)).toEqual(tokens)
+})
+
+it('ast', () => {
+  expect(parser(tokenizer(input))).toEqual(ast)
 })
