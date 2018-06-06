67acfad7ce0736792e9e53375c171e8c8dac69dc
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-06-06 22:36:21 +0800

    request payload init

--- learn-node/pirplets/main.ts
+++ learn-node/pirplets/main.ts
@@ -1,8 +1,35 @@
 import "babel-polyfill";
 
 import * as http from "http";
+import * as url from "url";
+import { StringDecoder } from "string_decoder";
 
 const httpServer = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  const queryStringObject = parsedUrl.query;
+
+  const method = req.method.toLowerCase();
+
+  const headers = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+
+  req.on("data", (data: Buffer) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    console.log(buffer);
+  });
+
+  console.log("Request headers: ", headers);
+
   res.end(`Hello world!`);
 });
 
